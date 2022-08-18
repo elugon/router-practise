3a91313d13d1f3f6c7eb51210ded4bdd072c1e01
Elu Gonzalez <elugon10@gmail.com>
2022-08-18 19:40:42 +0200

    lab done

--- src/views/Projects.jsx
+++ src/views/Projects.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Outlet, Link } from 'react-router-dom';
 
 export default function Projects() {
   return (
     <div>
       <h1>My projects</h1>
       <p>I have separated my projects into two categories: games I have designed and developed, and applications I have programmed.</p>
+      <button><Link to="/projects/projectsgames">See Games</Link></button>
+      <button><Link to="/projects/projectsapps">See Apps</Link></button>
+      <Outlet />
     </div>
   )
 }
